d0e1463adaa03929ab9131d9b98c3f9072435656
tmk <tmk@fastmail.fm>
2026-05-15 03:22:00 +0200

    feat: hosts/secondary-1.{nix,hardware.nix}

---
+++ hosts/secondary-1-hardware.nix
@@ -0,0 +1,8 @@
+throw ''
+  hosts/secondary-1-hardware.nix is a placeholder.
+
+  Run `nixos-anywhere --flake .#secondary-1-bootstrap \
+    --generate-hardware-config nixos-generate-config \
+    ./hosts/secondary-1-hardware.nix --target-host root@<ip>`
+  to populate it during the bootstrap install.
+''
---
+++ hosts/secondary-1.nix
@@ -0,0 +1,48 @@
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  # Hardware-config import is composed by flake.nix's helpers, not here.
+  # See hosts/canonical.nix for the rationale.
+  imports = [
+    ../modules/common.nix
+    ../modules/disko.nix
+    ../modules/fossil-server.nix
+  ];
+
+  networking.hostName = "secondary-1";
+
+  # Agenix-decrypted secrets this host consumes.
+  age.secrets = {
+    cloudflare-dns.file = ../secrets/cloudflare-dns.age;
+    fossil-sync = {
+      file = ../secrets/fossil-sync.age;
+      owner = "fossil";
+      group = "fossil";
+      mode = "0440";
+    };
+    tmk-password.file = ../secrets/tmk-password.age;
+    "tailscale-authkey-secondary-1".file = ../secrets/tailscale-authkey-secondary-1.age;
+    "healthchecks-secondary-1" = {
+      file = ../secrets/healthchecks-secondary-1.age;
+      owner = "fossil";
+      group = "fossil";
+      mode = "0440";
+    };
+  };
+
+  services.tailscale = {
+    enable = true;
+    authKeyFile = config.age.secrets."tailscale-authkey-secondary-1".path;
+  };
+
+  users.users.tmk.hashedPasswordFile = config.age.secrets.tmk-password.path;
+
+  services.fossilServer = {
+    enable = true;
+    role = "secondary";
+    domain = "s1.fossil.exidia.com";
+    healthcheckUrlFile = config.age.secrets."healthchecks-secondary-1".path;
+  };
+
+  system.stateVersion = "26.05";
+}
