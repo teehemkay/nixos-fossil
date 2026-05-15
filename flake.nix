b52fb13511ee004a2854b5b5fec96081f4291727
tmk <tmk@fastmail.fm>
2026-05-15 02:13:41 +0200

    feat: initial flake.nix with nixpkgs + disko + agenix inputs

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "NixOS fossil-server cluster: canonical + 2 secondaries";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, disko, agenix, ... }: {
+    # nixosConfigurations populated by later tasks
+    nixosConfigurations = { };
+  };
+}
